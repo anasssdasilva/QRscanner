9c3bce29e237c7fb41a86083e26855db460889aa
Khaoula Msis <mkhaoula36@gmail.com>
2022-01-28 17:28:22 +0100

    first commit

---
+++ app/src/main/java/com/example/qrcodescanner/MainActivity.java
@@ -0,0 +1,223 @@
+package com.example.qrcodescanner;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.example.qrcodescanner.beans.Crenau;
+import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken;
+import com.google.zxing.Result;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Type;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    StringRequest request;
+    String insertUrll = "https://gestion-salles-blocs-ak.herokuapp.com/crenau/api";
+    String insertUrl = "https://gestion-salles-blocs-ak.herokuapp.com/occupation/apic";
+    private ArrayList<Crenau> crenau;
+
+    private CodeScanner mCodeScanner;
+    boolean CameraPermission = false;
+    final int CAMERA_PERM = 1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this,scannerView);
+        askPermission();
+
+        if (CameraPermission) {
+
+            scannerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    mCodeScanner.startPreview();
+
+                }
+            });
+
+            mCodeScanner.setDecodeCallback(new DecodeCallback() {
+                @Override
+                public void onDecoded(@NonNull @NotNull Result result) {
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            String time;
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+                            String currentDateandTime = sdf.format(new Date());
+                            System.out.println(sdf);
+                            Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
+
+
+                            Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                            System.out.println(crenau+"ghvggvv");
+
+                            Bundle bundle = new Bundle();
+
+                            bundle.putParcelableArrayList("MYLIST",  crenau);
+                            intent.putExtras(bundle);
+
+
+                            intent.putExtra("idSalle",result.getText());
+                            startActivity(intent);
+
+                        }
+                    });
+
+                }
+            });
+
+        }
+    }
+
+
+    private void askPermission(){
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+
+
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ){
+
+                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},CAMERA_PERM);
+
+
+            }else {
+
+                mCodeScanner.startPreview();
+                CameraPermission = true;
+            }
+
+        }
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @NotNull String[] permissions, @NonNull @NotNull int[] grantResults) {
+
+        if (requestCode == CAMERA_PERM){
+
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                mCodeScanner.startPreview();
+                CameraPermission = true;
+            }else {
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.CAMERA)){
+
+                    new AlertDialog.Builder(this)
+                            .setTitle("Permission")
+                            .setMessage("Please provide the camera permission for using all the features of the app")
+                            .setPositiveButton("Proceed", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},CAMERA_PERM);
+
+                                }
+                            }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+
+                        }
+                    }).create().show();
+
+                }else {
+
+                    new AlertDialog.Builder(this)
+                            .setTitle("Permission")
+                            .setMessage("You have denied some permission. Allow all permission at [Settings] > [Permissions]")
+                            .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                    dialog.dismiss();
+                                    Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                                            Uri.fromParts("package",getPackageName(),null));
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                    finish();
+
+
+                                }
+                            }).setNegativeButton("No, Exit app", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+                            finish();
+
+                        }
+                    }).create().show();
+
+
+
+                }
+
+            }
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onPause() {
+        if (CameraPermission){
+            mCodeScanner.releaseResources();
+        }
+
+        super.onPause();
+    }
+}
